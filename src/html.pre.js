92866b3106fcb7a061a796668f055213a057feb7
Alexandre Capt <acapt@adobe.com>
2018-09-13 10:49:39 +0200

    Implemented sections

--- src/html.pre.js
+++ src/html.pre.js
@@ -16,9 +16,12 @@
  *
  */
 const select = require('unist-util-select');
-const visit  = require('unist-util-visit');
+const visit = require('unist-util-visit');
 const toHAST = require('mdast-util-to-hast');
 const toHTML = require('hast-util-to-html');
+const mdastSqueezeParagraphs = require('mdast-squeeze-paragraphs');
+const mdastFlattenImages = require('mdast-flatten-image-paragraphs');
+const mdastFlattenLists = require('mdast-flatten-listitem-paragraphs');
 
 
 /**
@@ -31,65 +34,132 @@ const LayoutMachine = {
       init -> hero, flow
       hero -> flow
   */
-  validStates: ['hero', 'flow'],
+  validStates: ['hero', 'flow', 'gallery'],
   states: ['init'],
-  get state(){
+  get state() {
     return this.states[this.states.length - 1];
   },
-  set state(v){
+  set state(v) {
     console.log(`${this.state} -> ${v}`);
 
     this.states.push(v);
     return v;
   },
-  layout: function(section){
+  layout: function (section) {
     // allow manual overide of class
     // this might be instant-cruft–discuss.
     if (section.class && section.class.length) {
       // If class is a valid state, use it, otherwise default to 'flow'
-      if (this.validStates.includes(section.class)){
+      if (this.validStates.includes(section.class)) {
         this.states.push(section.class);
-      }
-      else{
+      } else {
         this.states.push('flow');
       }
-    }
-    else{
-      switch(this.state){
+    } else {
+      switch (this.state) {
         case 'init':
-          // If the section has an h2 & an image, it's a hero
-          let image = select(section, 'image');
-          let h = select(section, 'heading');
-          if (h.length == 1 && h[0].depth == 2 && image.length == 1){
-            h = h[0];
+          if (this.isHero(section)) {
             this.state = 'hero';
-            section.children = [h];
-            section.type = 'standard';
-            section.style = `background-image: url("#{image});`;
-
-            break;
+          } else {
+            if (this.isGallery(section)) {
+              this.state = 'gallery';
+            } else {
+              this.state = 'flow';
+            }
           }
-          else{
-            this.state = 'flow';
-            break;
-          }
+          break;
+        case 'flow':
         case 'hero':
-          this.state = 'flow';
+          if (this.isGallery(section)) {
+            this.state = 'gallery';
+          } else {
+            this.state = 'flow';
+          }
           break;
       }
       section.class = this.state;
     }
 
     let children = [];
-    for (let e of section.children){
-      children.push(toHTML(toHAST(e)));
+    for (let e of section.children) {
+      children.push(e);
     }
     section.children = children;
     return section;
   },
-  get hasHero(){
+
+  get hasHero() {
     return this.states.includes('hero');
-  }
+  },
+
+  isHero(section) {
+    // If the section has an h2 & an image in the first level, it's a hero
+    const image = select(section, 'image');
+    const h = select(section, 'heading');
+    return (h.length == 1 && (h[0].depth == 1 || h[0].depth == 2) && image.length == 1);
+  },
+
+  isGallery(section) {
+    // If the section has more than 2 images, it is a gallery
+    const image = select(section, 'image');
+    return image.length > 2
+  },
+}
+
+function getSmartDesign(mdast) {
+  mdast = mdastFlattenImages()(mdast);
+  mdast = mdastFlattenLists()(mdast);
+  mdast = mdastSqueezeParagraphs(mdast);
+
+  const mdastNodes = mdast.children;
+
+  const sections = [];
+  let currentSection = {
+    children: [],
+    type: 'standard'
+  };
+
+  let title;
+
+  mdastNodes.forEach(function (node) {
+    if (node.type == "heading" && node.depth == 1 && !title) {
+      title = node.children[0].value;
+      return;
+    }
+    if (node.type == "thematicBreak") {
+      sections.push(LayoutMachine.layout(currentSection));
+      currentSection = {
+        children: [],
+        type: 'standard'
+      };
+    } else {
+      currentSection.children.push(node);
+    }
+  });
+
+  sections.push(LayoutMachine.layout(currentSection));
+  return sections;
+}
+
+function computeSectionsHAST(sections) {
+  const nodes = [];
+  let odd = false;
+  sections.forEach(function (section) {
+    const hast = toHAST(section);
+    const htmlNodes = [];
+    hast.children.forEach(function (h) {
+      htmlNodes.push(toHTML(h));
+    });
+    nodes.push({
+      type: "element",
+      properties: {
+        className: section.class + ' ' + ((odd = !odd) ? 'odd' : 'even'),
+      },
+      tagName: 'section',
+      children: hast.children
+    });
+  });
+  return nodes;
 }
 
 /**
@@ -98,8 +168,6 @@ const LayoutMachine = {
  * @param payload.resource The content resource
  */
 function pre(payload) {
-
-  let title = null;
 
   // banner is first image and banner text is image alt
   payload.resource.banner = {
@@ -114,27 +182,14 @@ function pre(payload) {
     }
   }
 
-  const content = [];
-  let currentSection = {children: [], type: 'standard'};
-
-  visit(payload.resource.mdast, ["paragraph","heading","thematicBreak"], function (node) {
-
-    if (node.type == "heading" && node.depth == 1 && !title){
-      title = node.children[0].value;
-      return;
-    }
-    if (node.type == "thematicBreak") {
-      content.push(LayoutMachine.layout(currentSection));
-      currentSection = {children: [], type: 'standard'};
-    }
-    else {
-      currentSection.children.push(node);
-    }
-  });
-
-  content.push(LayoutMachine.layout(currentSection));
-
-  payload.resource.content = content;
+  const sections = getSmartDesign(payload.resource.mdast);
+  console.log('sections', sections);
+  const sectionsHAST = computeSectionsHAST(sections);
+  const html = toHTML({
+    type: 'root',
+    children: sectionsHAST
+  })
+  payload.resource.sections = html;
 
   // avoid htl execution error if missing
   payload.resource.meta = payload.resource.meta || {};
